c3021db06fb1f187ef5afe68d8181d16b40e971b
MDGrey33 <roland@abouyounes.com>
2025-01-07 19:39:41 +0200

    feat: Added parallel processing of pptx files issue https://github.com/MDGrey33/pyvisionai/issues/14

--- pyvisionai/extractors/__init__.py
+++ pyvisionai/extractors/__init__.py
@@ -3,11 +3,13 @@
 from pyvisionai.extractors.pdf import PDFTextImageExtractor
 from pyvisionai.extractors.pdf_page import PDFPageImageExtractor
 from pyvisionai.extractors.docx import DocxTextImageExtractor
+from pyvisionai.extractors.docx_page import DocxPageImageExtractor
 from pyvisionai.extractors.pptx import PptxTextImageExtractor
 
 __all__ = [
     "PDFTextImageExtractor",
     "PDFPageImageExtractor",
     "DocxTextImageExtractor",
+    "DocxPageImageExtractor",
     "PptxTextImageExtractor",
 ]
